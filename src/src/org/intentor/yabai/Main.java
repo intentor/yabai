6fb4f16b3f98b2c9f474d8a6230642606f5256b9
André Intentor Martins <andre@intentor.com.br>
2015-10-10 10:05:23 -0300

    Updated project version.

--- src/src/org/intentor/yabai/Main.java
+++ src/src/org/intentor/yabai/Main.java
@@ -13,7 +13,7 @@ import org.intentor.yabai.valueobjects.AiParameters;
  */
 public class Main {
 	/** Program version. */
-	public static final String VERSION = "0.1";
+	public static final String VERSION = "0.2";
 	
 	public static void main(String[] args) {		
 		FileManager file = new FileManager(FileName.DATA_FILE_NAME);
